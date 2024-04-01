c3a5d5618c9108f5aa0e62a663a9533167b32106
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-04-01 08:47:26 +0200

    working bot

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,3 +1,5 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 const fs = require("fs");
 
 module.exports = (client) => {
@@ -18,6 +20,18 @@ module.exports = (client) => {
 					`Command: ${command.data.name} has been passed through the handler.`
 				);
 			}
+		}
+
+		const clientId = "1012693236541829147";
+		const rest = new REST({ version: "9" }).setToken(process.env.token);
+		try {
+			console.log("Started refreshing application (/) commands.");
+
+			await rest.put(Routes.applicationCommands(clientId), {
+				body: client.commandArray,
+			});
+		} catch (error) {
+			console.error(error);
 		}
 	};
 };
