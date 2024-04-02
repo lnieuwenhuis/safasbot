063fc8d22e86a1ad145cf97ca79dba4e146d156e
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-04-02 11:23:37 +0200

    custom status + purge edits

--- src/commands/tools/purge.js
+++ src/commands/tools/purge.js
@@ -20,23 +20,37 @@ module.exports = {
 		),
 
 	async execute(interaction, client) {
+		const numberToDelete = interaction.options.getNumber("number");
+
 		const confirm = new ButtonBuilder()
 			.setCustomId("confirm")
-			.setLabel("Confirm Ban")
+			.setLabel("Confirm Purge")
 			.setStyle(ButtonStyle.Danger);
 
 		const cancel = new ButtonBuilder()
 			.setCustomId("cancel")
-			.setLabel("Cancel")
+			.setLabel("Cancel Purge")
 			.setStyle(ButtonStyle.Secondary);
 
 		const row = new ActionRowBuilder().addComponents(confirm, cancel);
+		var embed;
 
-		const embed = new EmbedBuilder()
-			.setColor("#0099ff")
-			.setTitle("title")
-			.setDescription("description")
-			.addFields({ name: "title", value: "value", inline: false });
+		if (numberToDelete) {
+			embed = new EmbedBuilder()
+				.setColor("#0099ff")
+				.setTitle(
+					`Are you sure you want to Delete ${numberToDelete} Messages?`
+				)
+				.setDescription("Use the Buttons Below")
+				.addFields({ name: "title", value: "value", inline: false });
+		} else {
+			embed = new EmbedBuilder()
+				.setColor("#0099ff")
+				.setTitle(
+					"Are you sure you want to Delete the Entire Channel History?"
+				)
+				.setDescription("Use the Buttons Below");
+		}
 
 		await interaction
 			.reply({ embeds: [embed], components: [row] })
--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,15 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
 	name: "ready",
 	once: true,
 	async execute(client) {
+		client.user.setActivity({
+			type: ActivityType.Custom,
+			name: "customstatus",
+			state: "github.com/lnieuwenhuis/discordbotjs",
+		});
+
 		console.log(`Ready, ${client.user.tag} is logged in and online!`);
 	},
 };
