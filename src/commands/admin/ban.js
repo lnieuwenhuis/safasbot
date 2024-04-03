2429dfdbd5e91be0ca6cfcf47f931dee81064a74
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-04-03 14:26:28 +0200

    infra + ban scaffolding

---
+++ src/commands/admin/ban.js
@@ -0,0 +1,41 @@
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	PermissionsBitField,
+	PermissionFlagsBits,
+} = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("ban")
+		.setDescription(`Ban a member from the server`)
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+		.addMentionableOption((option) =>
+			option
+				.setName(`member`)
+				.setDescription(`The member to ban from the server`)
+				.setRequired(true)
+		),
+
+	async execute(interaction, client) {
+		if (
+			!interaction.member.permissions.has(
+				PermissionsBitField.Flags.BanMembers
+			)
+		)
+			return interaction.reply({
+				content: "You don't have permission to do that!",
+				ephemeral: true,
+			});
+
+		const memberId = interaction.options.get(`member`).value;
+		const memberName =
+			interaction.guild.members.cache.get(memberIdToBan).user.globalName;
+
+		return interaction.reply({ content: "Member sent to console" });
+	},
+};
